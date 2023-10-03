f0bcde4dc4d83fb3ccb6aa44057c54f423194143
farzamsultani <farzamsaber03@gmail.com>
2023-10-03 11:23:25 -0700

    learnd loops and switch

---
+++ day2.js
@@ -0,0 +1,32 @@
+// let sim= 93;
+// switch(sim){
+//     case 1:
+//         console.log('its USA phone number')
+//         break;
+//         case 93:
+//             alert('Its AFG number')
+//             break;
+//             case 44:
+//                 console.log('IDK where it is from')
+//                 break;
+//                 default:
+//                     console.log('Invalid Number')
+//}
+let i = 1;
+for( i ;i < 10;i++){
+ console.log(`5 x ${i} = ${5 * i}`);
+}
+
+let j = 2;
+for(j; j < 10; j++){
+    console.log(`2 x ${i}=${2 * i}`);
+}
+let person={ age:23,Fname:'jhon',lname:'david' };
+for(let x in person){
+    console.log(person[x]);
+}
+let students={name:'ali',lname:'Ahmad'};
+for(let y in students)
+{
+    console.log(students[y]);
+}
