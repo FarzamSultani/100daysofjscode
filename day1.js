14592eeb20e6052bddb1c39478c682e260ea51dd
farzamsultani <farzamsaber03@gmail.com>
2023-10-03 10:35:10 -0700

    testing

---
+++ day1.js
@@ -0,0 +1,24 @@
+//alert('hello world')
+/*let message;
+message='hello this is let';
+
+let user='Farzam',age=24,id='20c';
+var call='Go study you';
+
+const my_BD='2003,4,6';
+console.log(user,age,id,call,my_BD);*/
+////////////////////Day 2 of 100 days code///////////////////////////////////////////////
+//prompt("NAME","Farzam")
+//prompt("size","Extra_Large")
+//confirm("Do You want to be top 1%")
+
+let num=20;
+if (num > 10){
+    console.log('you are great ok')
+}
+else if(num < 10) {
+  console.log('go fuckyour self');
+}
+else{
+    console.log('sorry try again later');
+}
