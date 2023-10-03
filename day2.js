1ce01085f305566042531dc895ec79b6fe5dece1
farzamsultani <farzamsaber03@gmail.com>
2023-10-03 11:32:57 -0700

    added some codes

--- day2.js
+++ day2.js
@@ -12,21 +12,25 @@
 //                 default:
 //                     console.log('Invalid Number')
 //}
-let i = 1;
-for( i ;i < 10;i++){
- console.log(`5 x ${i} = ${5 * i}`);
-}
+// let i = 1;
+// for( i ;i < 10;i++){
+//  console.log(`5 x ${i} = ${5 * i}`);
+// }
 
-let j = 2;
-for(j; j < 10; j++){
-    console.log(`2 x ${i}=${2 * i}`);
-}
-let person={ age:23,Fname:'jhon',lname:'david' };
+// let j = 2;
+// for(j; j < 10; j++){
+//     console.log(`2 x ${i}=${2 * i}`);
+// }
+// let person={ age:23,Fname:'jhon',lname:'david' };
+// for(let x in person){
+//     console.log(person[x]);
+// }
+// let students={name:'ali',lname:'Ahmad'};
+// for(let y in students)
+// {
+//     console.log(students[y]);
+// }
+let person={fname:'ali',Lname:'khan',Mname:'sardar'};
 for(let x in person){
-    console.log(person[x]);
-}
-let students={name:'ali',lname:'Ahmad'};
-for(let y in students)
-{
-    console.log(students[y]);
+    alert(x+""+person[x])
 }
