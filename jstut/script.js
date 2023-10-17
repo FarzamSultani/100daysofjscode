9dc1fe09aa6eeac63d79e48cacb9c0937cba915c
farzamsultani <farzamsaber03@gmail.com>
2023-10-17 08:04:08 -0700

    add

--- jstut/script.js
+++ jstut/script.js
@@ -1,14 +1,33 @@
-//adding value to const object
+// 1 .... adding value to const object
 
-const stu ={
-    name : "farzam",
-    age:20,
+// const stu ={
+//     name : "farzam",
+//     age:20,
 
-}
+// }
 
-stu.fitness= "karim caoch";
-console.log(stu);
-stu.name="sami sardarzai"
-stu.fitness="sami"
-stu.fname="javad";
-console.log(stu);
+// stu.fitness= "karim caoch";
+// console.log(stu);
+// stu.name="sami sardarzai"
+// stu.fitness="sami"
+// stu.fname="javad";
+// console.log(stu);
+
+//2 .... Making a dic
+
+// const words = {
+//     ketab:"book",
+//     dastan:"story",
+ 
+//   };
+
+//    //how to call
+
+//  console.log(words["dastan"]);  
+
+//3.... ternary operators
+
+// const age = 22;
+// const dofitness = age >= 22 ? "yes do 2 times in a day" : "no do not do fitness";
+// console.log(dofitness); // "Beer"
+
