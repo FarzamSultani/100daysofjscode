cc6ce8e8035555067479b7d5ee91732ea8b228a1
farzamsultani <farzamsaber03@gmail.com>
2023-10-17 07:30:57 -0700

    add new files

---
+++ jstut/script.js
@@ -0,0 +1,14 @@
+//adding value to const object
+
+const stu ={
+    name : "farzam",
+    age:20,
+
+}
+
+stu.fitness= "karim caoch";
+console.log(stu);
+stu.name="sami sardarzai"
+stu.fitness="sami"
+stu.fname="javad";
+console.log(stu);
